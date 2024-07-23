e54c2aacc78e0c15ba16a9cab4df397631232249
Dmitriy <dmitriykharitonov333@gmail.com>
2024-07-23 18:55:56 +0300

    Создал класс Laptop

--- src/main/java/Laptop.java
+++ src/main/java/Laptop.java
@@ -1,2 +1,62 @@
-public class Laptop {
+/**
+ * Класс Laptop
+ */
+class Laptop {
+    private String brand;
+    private int ram;
+    private int hdd;
+    private String os;
+    private String color;
+    private double price;
+
+    /**
+     * Конструктор класса Laptop
+     * @param brand модель ноутбука
+     * @param ram объем оперативной памяти в ГБ
+     * @param hdd объем жесткого диска в ГБ
+     * @param os название операционной системы
+     * @param color цвет ноутбука
+     * @param price цена ноутбука
+     */
+    public Laptop(String brand, int ram, int hdd, String os, String color, double price) {
+        this.brand = brand;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "model='" + brand + '\'' +
+                ", ram=" + ram +
+                ", hdd=" + hdd +
+                ", os='" + os + '\'' +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
+
